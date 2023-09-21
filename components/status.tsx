d9575474205c9196e26ae6e25fec2e09d2af4cc7
MugiSus <mmgiss256@gmail.com>
2023-09-22 01:52:37 +0900

    wrap fontawesome icon

--- components/status.tsx
+++ components/status.tsx
@@ -10,7 +10,9 @@ export default function Status({
 }) {
   return (
     <li className="flex place-items-center flex-row gap-2">
-      <FontAwesomeIcon icon={icon} className="text-primary h-4" />
+      <div className="h-4">
+        <FontAwesomeIcon icon={icon} className="text-primary h-4" />
+      </div>
       <div className="text-center font-medium">{children}</div>
     </li>
   );
