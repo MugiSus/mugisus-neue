31e052b32dd68dfcf2cda0453c14b2f09750b878
MugiSus <mmgiss256@gmail.com>
2023-10-14 03:51:03 +0900

    interactive image

--- components/InteractiveIcon.tsx
+++ components/InteractiveIcon.tsx
@@ -2,15 +2,47 @@
 
 import Image from "next/image";
 
+function ImageFragments({
+  src,
+  className,
+}: {
+  src: string;
+  className: string;
+}) {
+  return (
+    <Image
+      src={src}
+      width={320}
+      height={320}
+      alt="mugisus's icon"
+      draggable="false"
+      className={`absolute transition-all ${className}`}
+    />
+  );
+}
+
 export default function InteractiveIcon() {
   return (
-    <div className="relative w-80 h-80">
-      <Image
-        src="oyagisvg_wireframe_all.svg"
-        width={300}
-        height={300}
-        alt="mugisus's icon"
-        className="transition-all duration-500 hover:translate-y-1"
+    <div className="relative w-80 h-80 group">
+      <ImageFragments
+        src="/oyagi_wireframes/variant=body.svg"
+        className="duration-500 group-hover:translate-y-0.5"
+      />
+      <ImageFragments
+        src="/oyagi_wireframes/variant=head.svg"
+        className="duration-500 group-hover:translate-y-0.5 group-active:translate-y-[3px]"
+      />
+      {/* <ImageFragments
+        src="/oyagi_wireframes/variant=head-closed.svg"
+        className="duration-500 group-hover:translate-y-0.5 group-active:translate-y-[3px]"
+      />
+      <ImageFragments
+        src="/oyagi_wireframes/variant=head-half.svg"
+        className="duration-500 group-hover:translate-y-0.5 group-active:translate-y-[3px]"
+      /> */}
+      <ImageFragments
+        src="/oyagi_wireframes/variant=cap.svg"
+        className="duration-500 group-hover:translate-y-1 group-active:translate-y-2"
       />
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      use: "js-yaml-loader",
-    });
+    config.module.rules.push(
+      {
+        test: /\.ya?ml$/,
+        use: "js-yaml-loader",
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      }
+    );
     return config;
   },
 };
