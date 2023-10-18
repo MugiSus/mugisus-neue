cc8da010f0514f3c948a745538ac984ae9f20307
MugiSus <mmgiss256@gmail.com>
2023-10-19 03:35:11 +0900

    change hover animation duration

--- components/creationItem.tsx
+++ components/creationItem.tsx
@@ -18,14 +18,16 @@ export default function CreationItem({
 
   const content = (
     <li
-      className={`flex px-32 py-16 gap-1 place-items-center flex-col rounded-lg max-w-full transition-all duration-500 hover:-translate-y-1 hover:shadow-xl ${
+      className={`flex group px-32 py-16 gap-1 place-items-center flex-col rounded-lg max-w-full transition-all duration-1000 hover:duration-200 hover:-translate-y-1 hover:shadow-xl hover:bg-stone-50 ${
         ["bg-creation-light", "bg-creation-dark"][index % 2]
       }`}
     >
-      <div className="text-center whitespace-nowrap">{creation.title}</div>
+      <div className="text-center whitespace-nowrap transition-colors duration-1000 group-hover:duration-200 group-hover:text-stone-800">
+        {creation.title}
+      </div>
       <time
         dateTime={creation.date.toISOString()}
-        className="text-primary text-xs"
+        className="text-primary text-xs transition-colors duration-1000 group-hover:duration-200 group-hover:text-stone-500"
       >
         {dateString}
       </time>
