d7360ae6b1e95056c9c3a71448502a2fb3bf56e6
MugiSus <mmgiss256@gmail.com>
2023-10-19 04:53:57 +0900

    disable elevating

--- components/creationItem.tsx
+++ components/creationItem.tsx
@@ -18,16 +18,16 @@ export default function CreationItem({
 
   const content = (
     <li
-      className={`flex group px-32 py-16 gap-1 place-items-center flex-col rounded-lg max-w-full transition-all duration-1000 hover:duration-200 hover:-translate-y-1 hover:shadow-xl hover:bg-stone-50 ${
+      className={`flex group px-32 py-16 gap-1 place-items-center flex-col rounded-lg max-w-full transition-all duration-300 hover:bg-stone-50 ${
         ["bg-creation-light", "bg-creation-dark"][index % 2]
       }`}
     >
-      <div className="text-center whitespace-nowrap transition-colors duration-1000 group-hover:duration-200 group-hover:text-stone-800">
+      <div className="text-center whitespace-nowrap transition-colors duration-300 group-hover:text-stone-800">
         {creation.title}
       </div>
       <time
         dateTime={creation.date.toISOString()}
-        className="text-primary text-xs transition-colors duration-1000 group-hover:duration-200 group-hover:text-stone-500"
+        className="text-primary text-xs transition-colors duration-300 group-hover:text-stone-500"
       >
         {dateString}
       </time>
