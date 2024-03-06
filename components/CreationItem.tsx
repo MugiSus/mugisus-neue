29db9e964a44ca8498a57267d5d3bf4e99d81dca
MugiSus <mmgiss256@gmail.com>
2024-03-06 13:37:48 +0900

    apply 3d effects

--- components/CreationItem.tsx
+++ components/CreationItem.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Creation } from "@/models/creations";
 import Link from "next/link";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
@@ -20,12 +22,26 @@ export default function CreationItem({
 
   const content = (
     <li
-      className={`flex relative group px-32 py-16 gap-0 place-items-center flex-col rounded-md max-w-full overflow-hidden transition-all duration-1000 hover:duration-0 hover:bg-neutral-700 ${
+      className={`flex relative cursor-none group px-32 py-16 gap-0 place-items-center flex-col rounded-md max-w-full overflow-hidden transition-all duration-500 hover:duration-100 hover:bg-neutral-700 ${
         index % 2 ? "bg-creation-dark" : "bg-creation-light"
       }`}
+      onMouseMove={(event) => {
+        // subtle 3D effect
+        const rect = event.currentTarget.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+        const rotateX = (centerY - y) * 0.05;
+        const rotateY = (centerX - x) * -0.05;
+        event.currentTarget.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(40px) scale(102%)`;
+      }}
+      onMouseLeave={(event) => {
+        event.currentTarget.style.transform = "rotateX(0deg) rotateY(0deg)";
+      }}
     >
       <div
-        className={`whitespace-nowrap leading-none tracking-tighter text-9xl duration-1000 group-hover:duration-0 group-hover:text-neutral-600 ${
+        className={`whitespace-nowrap leading-none tracking-tighter text-9xl duration-500 group-hover:duration-100 group-hover:text-neutral-600 ${
           index % 2 ? "text-creation-light" : "text-creation-dark"
         }`}
       >
@@ -48,18 +64,18 @@ export default function CreationItem({
           {creation.title.repeat(4)}
         </div>
       </div>
-      <div className="text-center whitespace-nowrap transition-colors duration-1000 group-hover:duration-0 group-hover:text-neutral-50 z-50">
+      <div className="text-center whitespace-nowrap transition-colors duration-500 group-hover:duration-100 group-hover:text-neutral-50 z-50">
         {creation.title}
       </div>
       <time
         dateTime={creation.date.toISOString()}
-        className="text-neutral-800 text-xs transition-colors duration-1000 group-hover:duration-0 group-hover:text-neutral-200 tabular-nums z-50"
+        className="text-neutral-800 text-xs transition-colors duration-500 group-hover:duration-100 group-hover:text-neutral-200 tabular-nums z-50"
       >
         {dateString}
       </time>
       <FontAwesomeIcon
         icon={faArrowRight}
-        className="opacity-0 absolute h-5 right-4 top-0 bottom-0 m-auto text-neutral-200 duration-1000 group-hover:duration-0 group-hover:opacity-100 transition-all"
+        className="opacity-0 absolute h-5 right-4 top-0 bottom-0 m-auto text-neutral-200 duration-500 group-hover:duration-100 group-hover:opacity-100 transition-all"
       />
     </li>
   );
