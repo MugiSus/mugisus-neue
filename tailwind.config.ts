16bc15bb0bbc90eb83987e8287086c05d46bee2e
MugiSus <mmgiss256@gmail.com>
2024-04-19 12:32:13 +0900

    harden content color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
         "more-widest": ".2em",
       },
       colors: {
-        content: "#606060",
+        content: "#505050",
         primary: "#51B660",
         "creation-light": "#F2F2F2",
         "creation-dark": "#E8E8E8",
