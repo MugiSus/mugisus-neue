2b74454c0e6378db746385f78cce809ceec15c66
MugiSus <mmgiss256@gmail.com>
2024-12-13 16:10:47 +0900

    separator: simplify border style for improved clarity

--- components/Separator.tsx
+++ components/Separator.tsx
@@ -1,5 +1,5 @@
 export default function Separator() {
   return (
-    <div className="w-16 max-w-5xl h-0 border-t-2 border-dashed border-neutral-400" />
+    <div className="w-16 max-w-5xl h-0 border-t border-dashed border-neutral-400" />
   );
 }
