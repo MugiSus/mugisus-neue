e9f6c256988475b001e5f16dc70cdd99ccaeb3ae
MugiSus <mmgiss256@gmail.com>
2024-12-14 17:34:57 +0900

    InteractiveIcon: update stroke colors to use currentcolor for better theming support

--- components/InteractiveIcon.tsx
+++ components/InteractiveIcon.tsx
@@ -40,7 +40,7 @@ export default function InteractiveIcon() {
   }, []);
 
   return (
-    <div className="flex items-center justify-center w-[300px] h-[300px] saturate-0 hover:saturate-100 duration-300 group select-none">
+    <div className="flex relative items-center justify-center w-[300px] h-[300px] text-content hover:text-primary duration-300 group select-none">
       <div className="absolute top-0 left-0 w-6 h-6 border-l border-t border-dashed border-content" />
       <div className="absolute bottom-0 right-0 w-6 h-6 border-r border-b border-dashed border-content" />
       <div className="absolute top-0 right-0 w-6 h-6 border-r border-t border-dashed border-content" />
